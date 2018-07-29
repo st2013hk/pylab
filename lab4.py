fed56fa728d41d3e23e3cd9da96948966a88512f
Steven <st2013hk@yahoo.com.hk>
2018-07-29 18:06:53 +0800

    3

--- lab4.py
+++ lab4.py
@@ -17,3 +17,4 @@
 # spam()
 # print(eggs)
 
+
---
+++ lab5.py
@@ -0,0 +1,29 @@
+# def spam():
+#     eggs = 99
+#     bacon()
+#     print(eggs)
+# def bacon():
+#     ham = 101
+#     eggs = 0
+# spam()
+# Global variable can be read from local scope
+# def spam():
+#     v1=eggs+10
+#     print(v1)
+# eggs = 42
+# spam()
+# print(eggs)
+
+#same name of global scope and local scope, print local scope variable first
+# def spam():
+#     eggs = 'spam local'
+#     print(eggs) # prints 'spam local'
+# def bacon():
+#     eggs = 'bacon local'
+#     print(eggs) # prints 'bacon local'
+#     spam()
+#     print(eggs) # prints 'bacon local'
+# eggs = 'global'
+# bacon()
+# print(eggs) # prints 'global'
+
