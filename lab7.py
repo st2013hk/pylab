f4e0fe34c2e4136339c290f1b87bf7c21aed8207
steven <st2013hk@yahoo.com.hk>
2018-07-30 23:42:34 +0800

    2

---
+++ lab7.py
@@ -0,0 +1,18 @@
+# spam = ['cat', 'bat', 'rat', 'elephant']
+# print(spam)
+# print(spam[0]+'\t')
+# print(spam[1]+'\t')
+# print(spam[2]+'\t')
+# print(spam[3]+'\t')
+
+spam = ['cat', 'bat', 'rat', 'elephant','dog']
+# print(spam[0][0])
+# print(spam[1][2])
+# print(spam[0:3])
+# slen=len(spam)
+# print(slen)
+
+spam[1] = 'aardvark'
+spam[2]=spam[1]
+spam[-1]=12345
+print(spam)
