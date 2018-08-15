35f8465d29f15c86733cdbfa35c910c9ebc93891
Steven <st2013hk@yahoo.com.hk>
2018-08-16 00:18:39 +0800

    10

--- plab/lab10.py
+++ plab/lab10.py
@@ -1,18 +1,18 @@
 dic = {
     '植物':
         {'草本植物':
-             ['牵牛花','瓜叶菊','葫芦','翠菊','冬小麦','甜菜'],
+             ['牵牛花', '瓜叶菊', '葫芦', '翠菊', '冬小麦', '甜菜'],
          '木本植物':
-             ['乔木','灌木','半灌木','如松','杉','樟'],
+             ['乔木', '灌木', '半灌木', '如松', '杉', '樟'],
          '水生植物':
-             ['荷花','千屈菜','菖蒲','黄菖蒲','水葱','再力花','梭鱼草']},
+             ['荷花', '千屈菜', '菖蒲', '黄菖蒲', '水葱', '再力花', '梭鱼草']},
     '动物':
         {'两栖动物':
-            ['山龟','山鳖','石蛙','娃娃鱼','蟾蜍','龟','鳄鱼','蜥蜴','蛇'],
+             ['山龟', '山鳖', '石蛙', '娃娃鱼', '蟾蜍', '龟', '鳄鱼', '蜥蜴', '蛇'],
          '禽类':
-            ['稚鸡','原鸡','长鸣鸡','昌国鸡','斗鸡','长尾鸡','乌骨鸡'],
+             ['稚鸡', '原鸡', '长鸣鸡', '昌国鸡', '斗鸡', '长尾鸡', '乌骨鸡'],
          '哺乳类动物':
-            ['虎','狼','鼠','鹿','貂','猴','貘','树懒','斑马','狗']}}
+             ['虎', '狼', '鼠', '鹿', '貂', '猴', '貘', '树懒', '斑马', '狗']}}
 
 li = []
 li2 = []
@@ -21,22 +21,48 @@ while go:
     for i, v in enumerate(dic, 1):
         print(i, v)
         li.append(v)
-    u_c = input(">>>")  # 1
-    u_c = int(u_c)
-    while go:
-        for i, v in enumerate(dic[li[u_c - 1]], 1):
-            print(i, v)
-            li2.append(v)
-        u_c2 = input(">>>>")  # 2
-        u_c2 = int(u_c2)
+    u_c = input("i,b,q")  # 1
+    if u_c.isdigit():
+        u_c = int(u_c)
         while go:
-            for v in enumerate(dic[li[u_c - 1]][li2[u_c2 - 1]]):
-                print(v)
-            u_c3 = str(input(">>>>>>"))  # b for back q for quit
-            u_c3 = u_c3.lower()
-            if u_c3 == "b":
-                li2.clear()
-                break
-            elif u_c3 == "q":
-                go = False
-                break
+            for i, v in enumerate(dic[li[u_c - 1]], 1):
+                print(i, v)
+                li2.append(v)
+            u_c2 = input(">>>>")  # 2
+            if u_c2.isdigit():
+                u_c2 = int(u_c2)
+                while go:
+                    for v in enumerate(dic[li[u_c - 1]][li2[u_c2 - 1]]):
+                        print(v)
+                    u_c3 = str(input(">>>>>>"))  # b for back q for quit
+                    u_c3 = u_c3.lower()
+                    if u_c3 == "b":
+                        li2.clear()
+                        break
+                    elif u_c3 == "q":
+                        go = False
+                        break
+            elif u_c2.isalpha():
+                u_c2 = u_c2.lower()
+                if u_c2 == "q":
+                    li.clear()
+                    break
+    elif u_c.isalpha():
+        u_c = u_c.lower()
+        if u_c == "i":
+            fl = input("Enter the type")
+            if dic.get(fl, 1) == 1:
+                dic[fl] = {}
+            f2 = input("Enter the detail type")
+            if dic[fl].get(f2, 1) == 1:
+                dic[fl][f2] = []
+            while True:
+                f3 = input("enter the name")
+                f3 = f3.lower()
+                if f3 == "q":
+                    li.clear()
+                    break
+                if f3 in dic[fl][f2]:
+                    print("exist")
+                else:
+                    dic[fl][f2].append(f3)
--- plab/lab11.py
+++ plab/lab11.py
@@ -1,15 +1,27 @@
-db = {
-}
+db = {}
 point = []
 while True:
     temp = db
     for item in point:
         temp = temp[item]
-    print(temp.keys())
+    print(list(temp.keys()))
     choice = input("Enter a choice, 1: insert 2:View 3:Quit 4:Back")
-    if choice=="1":
-        k=input("Enter a point")
-        if k in temp:
-            print("Item exist")
+    if choice == "1":
+        k = input("Enter a point")
+        if temp.get(k, 1) == 1:
+            temp[k] = {}
+        r = input("Enter a detail point")
+        if temp[k].get[r, 1] == 1:
+            temp[k][r] = []
+        s = input("Enter a name")
+        if s in temp[k][r]:
+            print("exist")
         else:
-            temp[k]
+            temp[k][r].append()
+
+    elif choice == "2":
+        k = input("Enter a point")
+        if k in temp:
+            print(list(temp.keys()))
+        else:
+            point.append(k)
