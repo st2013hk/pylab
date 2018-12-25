b15d675a284d947cfc3482a1a5a9bb8bc95c174e
Steven <st2013hk@yahoo.com.hk>
2018-12-26 00:38:11 +0800

    21

---
+++ chatbot/chat1.py
@@ -0,0 +1,43 @@
+from chatterbot import ChatBot
+chatbot = ChatBot("hellochatbot")
+# from chatterbot.trainers import ListTrainer
+
+# conversation = [
+#     "Hello",
+#     "Hi there!",
+#     "How are you doing?",
+#     "I'm doing great.",
+#     "That is good to hear",
+#     "Thank you.",
+#     "You're welcome."
+# ]
+#
+# chatbot.set_trainer(ListTrainer)
+# chatbot.train(conversation)
+# response = chatbot.get_response("what is your name")
+# print(response)
+
+bot = ChatBot(
+    'hellochatbot',
+    storage_adapter='chatterbot.storage.SQLStorageAdapter',
+    input_adapter='chatterbot.input.TerminalAdapter',
+    output_adapter='chatterbot.output.TerminalAdapter',
+    logic_adapters=[
+        'chatterbot.logic.MathematicalEvaluation',
+        'chatterbot.logic.TimeLogicAdapter'
+    ],
+    database='./mischat.db'
+)
+
+print("Type something to begin...")
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        # We pass None to this method because the parameter
+        # is not used by the TerminalAdapter
+        bot_input = bot.get_response(None)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
---
+++ execise/lab1.py
@@ -0,0 +1,17 @@
+# s = input("please enter a number")
+# s = int(s)
+# if s == 1:
+#     print('Hello')
+# elif s == 2:
+#     print('Howdy')
+# else:
+#     print('Greeting')
+
+# for s in range(1,11):
+#     print(s)
+
+# s = 1
+# while s < 11:
+#     print(s)
+#     s += 1
+abs(123)
