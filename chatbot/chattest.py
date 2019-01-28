56e81d52aa3809b24ba2dac210302c4c2f20ff15
Steven <st2013hk@yahoo.com.hk>
2019-01-29 00:02:23 +0800

    23

---
+++ chatbot/chattest.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
+
+#create a name for the bot
+bot = ChatBot('Halo')
+
+#Train the bot with base statement
+
+data = open('chatbotdata.txt').read()
+conversations = data.strip().split('\n')
+
+bot.set_trainer(ListTrainer)
+bot.train(conversations)
+#provide the question and chatbot answer
+while True:
+   try:
+      s=input('')
+      print('Steven:'+s+'')
+      response = bot.get_response(s)
+      print('HelpDesk:'+str(response))
+   except (KeyboardInterrupt, EOFError, SystemExit):
+      break
+
+
+
